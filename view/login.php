bb63eddafe9f821d05cdd7921283544a61e10834
ROCstudent <nathan.dousa@gmail.com>
2023-01-04 11:53:27 +0100

    css en flexbox beneden

--- view/login.php
+++ view/login.php
@@ -36,6 +36,24 @@ include('./view/header_home.php');
       <input type='text' id='user_lname' name='user_lname' roleholder='Achternaam' placeholder="Achternaam" required/>
       <input type='text' id='email' name='email' roleholder='email' placeholder="Email" required/>
       <input type='password' id='password' name='password' roleholder='Wachtwoord' placeholder="Wachtwoord" required/>
+      <select name="user_province" id="user_province">
+          <option value="Utrecht">Utrecht</option>
+          <option value="Noord-Holland">Noord-Holland</option>
+          <option value="Zuid-Holland">Zuid-Holland</option>
+          <option value="Friesland">Friesland</option>
+          <option value="Groningen">Groningen</option>
+          <option value="Drenthe">Drenthe</option>
+          <option value="Flevoland">Flevoland</option>
+          <option value="Overijssel">Overijssel</option>
+          <option value="Gelderland">Gelderland</option>
+          <option value="Limburg">Limburg</option>
+          <option value="Noord-Brabant">Noord-Brabant</option>
+          <option value="Zeeland">Zeeland</option>
+        </select>
+      <input type='text' id='city' name='city' roleholder='Stad' placeholder="Stad" required>
+      <input type='text' id='streetname' name='streetname' roleholder='Straatnaam' placeholder="Straatnaam" required>
+      <input type='text' id='postal_code' name='postal_code' roleholder='Postcode' placeholder="Postcode">
+      <input type='text' id='housenumber' name='housenumber' roleholder='Huisnummer' placeholder="Huisnummer" required>
       <button>Register</button>
     </form>
   </div>
